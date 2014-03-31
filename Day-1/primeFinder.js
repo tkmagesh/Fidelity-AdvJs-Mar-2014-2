8b677b7ee826632d93edc084d701b78402fc1e06
tkmagesh <tkmagesh77@gmail.com>
2014-03-31 17:11:54 +0530

    Prime Finder Caching Example - Closure Demonstration

---
+++ Day-1/primeFinder.js
@@ -0,0 +1,22 @@
+/*
+return if a given number is a prime number or not
+the result of a finding has to be cached so that you dont run the algorithm once again for the same number
+*/
+function getPrimeFinder(){
+   var cache = {0: false, 1: false, 2: true, 3:true};
+   function isPrime(n){
+       if (typeof cache[n] !== "undefined"){
+          console.log("from cache...");
+          return cache[n];
+       }
+       cache[n] = true;
+       for(var i=2;i<=(n/2);i++)
+           if (n % i === 0){
+              cache[n] = false;
+              break;
+           }
+       console.log("freshly brewed...");
+       return cache[n];
+   }
+   return isPrime;
+}
