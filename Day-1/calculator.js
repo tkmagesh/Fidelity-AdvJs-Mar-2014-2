3af84b74be1297ab17b029343822fccc9955318c
tkmagesh <tkmagesh77@gmail.com>
2014-03-31 12:27:01 +0530

    Calculator Demo - Part - 1

--- Day-1/calculator.js
+++ Day-1/calculator.js
@@ -1,3 +1,11 @@
 function sum(x,y){
-	return x + y;
+	function parseArg(n){
+		if (typeof n === "function") return parseArg(n());
+		if (isNaN(n)) return 0;
+		return parseInt(n);
+	}
+	var result = 0;
+	for(var i=0;i<arguments.length;i++)
+		result += parseArg(arguments[i]);
+	return result;
 }
