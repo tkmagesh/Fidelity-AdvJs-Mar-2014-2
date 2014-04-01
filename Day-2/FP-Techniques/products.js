12cdec423b9183178574000f0eecce31f7f1fb96
tkmagesh <tkmagesh77@gmail.com>
2014-04-01 10:45:14 +0530

    Comparer Functions - Demo

--- Day-2/FP-Techniques/products.js
+++ Day-2/FP-Techniques/products.js
@@ -6,3 +6,18 @@ var products = [
 	{id : 15, name : "ken", cost : 40, units : 50, category : 2},
 	{id : 10, name : "ten", cost : 10, units : 40, category : 1}
 ]
+
+function sort(list){
+  for(var i=0;i<list.length-1;i++)
+    for(var j=i+1;j<list.length;j++){
+       if (list[i].id > list[j].id){
+          var temp = list[i];
+          list[i] = list[j];
+          list[j] = temp;
+       }
+    }
+}
+
+//convert the above function in such a way that you can sort any list by any attribute
+
+sort(products);
